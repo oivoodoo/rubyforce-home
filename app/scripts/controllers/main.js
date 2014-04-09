baa4b96b113838a482c5cbc8a05ff1072f93b760
Artem Slabodnik <artem.slabodnik@gmail.com>
2014-04-10 01:42:08 +0300

    now we can use angular in our project

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('rubyforceHomeApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
