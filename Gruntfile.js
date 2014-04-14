77225c6df129b184a872afcf37540a9544d8efa0
Alexandr Korsak <alex.korsak@gmail.com>
2014-04-15 00:04:56 +0300

    Remove sass from bower-install task

--- Gruntfile.js
+++ Gruntfile.js
@@ -150,10 +150,6 @@ module.exports = function (grunt) {
       app: {
         src: ['<%= yeoman.app %>/index.html'],
         ignorePath: '<%= yeoman.app %>/'
-      },
-      sass: {
-        src: ['<%= yeoman.app %>/styles/{,*/}*.{scss,sass}'],
-        ignorePath: '<%= yeoman.app %>/bower_components/'
       }
     },
 
